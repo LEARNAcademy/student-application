6828daf4aac7e2e13af08c50c00a5ea7f795b8c1
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-03 16:24:21 -0700

    changed id values to match viable database values and fixed spacing between key values pairs to match json format

--- src/userMock.js
+++ src/userMock.js
@@ -1,65 +1,65 @@
 export const user = {
-  id: 0,
-  email: "manmountain@mountainman.com",
-  preferred_name: "Man Mountain",
+  id: 1,
+  email: "bob@bobemail.com",
+  preferred_name: "Bob",
   student: {
-    id: 0,
-    user_id: 0,
+    id: 1,
+    user_id: 1,
     absences: 3,
     assessment: [
       {
-        id: 0,
-        student_id: 0,
-        week:1,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 1"
+        id: 1,
+        student_id: 1,
+        week: 1,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 1"
       },
       {
-        id: 0,
-        student_id: 0,
-        week:2,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 2"
+        id: 1,
+        student_id: 1,
+        week: 2,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 2"
       },
       {
-        id: 0,
-        student_id: 0,
-        week:3,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 3"
+        id: 1,
+        student_id: 1,
+        week: 3,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 3"
       },
       {
-        id: 0,
-        student_id: 0,
-        week:4,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 4"
+        id: 1,
+        student_id: 1,
+        week: 4,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 4"
       },
       {
-        id: 0,
-        student_id: 0,
-        week:5,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 5"
+        id: 1,
+        student_id: 1,
+        week: 5,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 5"
       },
       {
-        id: 0,
-        student_id: 0,
-        week:6,
-        status:"complete",
-        comprehension:"competent",
-        reviewer:"someInstructor",
-        notes:"you need to spend more time on your assessments 6"
+        id: 1,
+        student_id: 1,
+        week: 6,
+        status: "complete",
+        comprehension: "competent",
+        reviewer: "Instructor",
+        notes: "you need to spend more time on your assessments 6"
       },
     ],
   },
