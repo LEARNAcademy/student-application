dd2912e32fe36e098b8b8b35c010d917a4dfbada
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-03 11:17:56 -0700

    completed mock data

---
+++ src/userMock.js
@@ -0,0 +1,66 @@
+export const user = {
+  id: 0,
+  email: "manmountain@mountainman.com",
+  preferred_name: "Man Mountain",
+  student: {
+    id: 0,
+    user_id: 0,
+    absences: 3,
+    assessment: [
+      {
+        id: 0,
+        student_id: 0,
+        week:1,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 1"
+      },
+      {
+        id: 0,
+        student_id: 0,
+        week:2,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 2"
+      },
+      {
+        id: 0,
+        student_id: 0,
+        week:3,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 3"
+      },
+      {
+        id: 0,
+        student_id: 0,
+        week:4,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 4"
+      },
+      {
+        id: 0,
+        student_id: 0,
+        week:5,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 5"
+      },
+      {
+        id: 0,
+        student_id: 0,
+        week:6,
+        status:"complete",
+        comprehension:"competent",
+        reviewer:"someInstructor",
+        notes:"you need to spend more time on your assessments 6"
+      },
+    ],
+  },
+};
