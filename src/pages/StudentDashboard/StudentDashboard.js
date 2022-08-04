750caf324a3e80f3e3a8e554e911106b212faecd
Renzlawr <RSLogan23@gmail.com>
2022-08-04 17:08:24 -0600

    added jsx-quotes, added prettier style guide for linter

--- src/pages/StudentDashboard/StudentDashboard.js
+++ src/pages/StudentDashboard/StudentDashboard.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const StudentDashboard = () => {
   return (
-    <>
+    <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
       <h3 className="underline">LEARN Student Application</h3>
-    </>
+    </div>
   )
 }
 
