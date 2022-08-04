fc95ef45d0b020b8061fc7060cca8f64d64efacf
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-04 11:57:22 -0700

    user can see default status of unassigned assessments for all weeks

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Assessment = (props) => {
+  const [assess, setAssess] = useState([
+    { week: 1, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' },
+    { week: 2, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' },
+    { week: 3, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' },
+    { week: 4, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' },
+    { week: 5, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' },
+    { week: 6, status: 'unassigned', comprehension: 'N/A', reviewer: 'N/A', notes: 'N/A' }
+  ])
   const { assessments } = props
+  const newAssess = assess.slice(assessments.length)
+  useEffect(() => {
+    setAssess(assessments.concat(newAssess))
+  }, [])
   return (
     <>
-      {assessments.map((assessment, index) => {
+      {assess.map((assessment, index) => {
         return (
-            <div key={index}>
-              <div> Week {assessment.week} </div>
-              <div> Status: {assessment.status} </div>
-              <div> Comprehension: {assessment.comprehension}</div>
-              <div> Reviewer: {assessment.reviewer} </div>
-              <div> Notes: {assessment.notes} </div>
-            </div>
+          <div key={index}>
+            <div> Week {assessment.week} </div>
+            <div> Status: {assessment.status} </div>
+            <div> Comprehension: {assessment.comprehension}</div>
+            <div> Reviewer: {assessment.reviewer} </div>
+            <div> Notes: {assessment.notes} </div>
+          </div>
         )
       })}
     </>
