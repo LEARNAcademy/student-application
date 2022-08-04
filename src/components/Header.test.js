dcf3c22ff5f4fd4def6a98622f44cf807f287b7d
LoneWanderer101 <sgtlinares@gmail.com>
2022-08-04 15:20:48 -0700

    testing for header

---
+++ src/components/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { Header } from './Header'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("When Header renders", () => {
+    let headerRender
+    beforeEach (() => {
+        headerRender = shallow(<Header />)
+    })
+  it('displays an image ', () => {
+    const reactHeaderLogo = headerRender.find('img')
+    expect(reactHeaderLogo.length).toEqual(1)
+  })
+})
