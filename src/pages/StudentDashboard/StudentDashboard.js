304f95a9fe70aab7304bea90098bebf5d73e0284
Davon <davonk12@hotmail.com>
2022-08-05 11:29:10 -0700

    Attendance policy complete

--- src/pages/StudentDashboard/StudentDashboard.js
+++ src/pages/StudentDashboard/StudentDashboard.js
@@ -3,14 +3,17 @@ import { Assessment } from "../../components/Assessment"
 import { Greeter } from "../../components/Greeter"
 import { Header } from "../../components/Header"
 import { user } from "../../userMock.js"
+import { AttendancePolicyModal } from "../../components/AttendancePolicyModal.js"
 
 const StudentDashboard = () => {
   const { assessments } = user.student
   return (
     <>
       <Header/>
+      <AttendancePolicyModal/>
       <Greeter user={user} />
       <Assessment assessments={assessments}/>
+    
     </>
   )
 }
