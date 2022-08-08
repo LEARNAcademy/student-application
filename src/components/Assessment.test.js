05e08e92ccf724667b54359c95b4349b3848057d
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-08 15:23:47 -0700

    pushing so multiple people can work on fixing testing

--- src/components/Assessment.test.js
+++ src/components/Assessment.test.js
@@ -2,12 +2,14 @@ import React from "react"
 import Enzyme, { shallow } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 import { Assessment } from "./Assessment"
+import { user } from "../userMock.js"
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("<Assessment />", () => {
   it("displays 6 divs with id of week", () => {
-    const assessmentsProps = []
+    const assessmentsProps = user.student.assessments
+    console.log(assessmentsProps)
     const assessment = shallow(<Assessment assessments={assessmentsProps}/>)
     const assessmentDiv = assessment.find("[id=\"week\"]")
     expect(assessmentDiv.length).toEqual(6)
