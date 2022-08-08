1c210725d9dd8ce86bca2e852a09678b7fc5e715
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-08 15:12:23 -0700

    finished styling and colored status indicator

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -1,47 +1,49 @@
 import React, { useState, useEffect } from "react"
 
 const Assessment = (props) => {
-  const [assess, setAssess] = useState([
-    { week: 1, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" },
-    { week: 2, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" },
-    { week: 3, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" },
-    { week: 4, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" },
-    { week: 5, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" },
-    { week: 6, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" }
-  ])
-
   const { assessments } = props
-  const newAssess = assess.slice(assessments.length)
-  useEffect(() => {
-    setAssess(assessments.concat(newAssess))
-  }, [])
 
-  const [statusColor, setStatusColor] = useState(" bg-[gray]")
+  const [statusColor, setStatusColor] = useState([])
+  const [assess, setAssess] = useState([])
 
-  const handleStatusColor = (assessment) => {
-    let status = assessment.status
-
-    status === "pending" ? 
-      () => setStatusColor(" bg-[yellow]") 
-      : 
-      status === "complete" ? 
-        () => setStatusColor(" bg-[green]") 
-        :
-        status === "incomplete" ? 
-          () => setStatusColor(" bg-[red]") 
-          :
-          status === "unassigned" ? 
-            () => setStatusColor(" bg-[gray]") 
-            : 
-            () => setStatusColor(" bg-[black")
+  const assessmentsUpdate = () => {
+    const newAssess = []
+    for(let i = 0; i < 6 ; i++) {
+      if(assessments[i].week === i + 1) {
+        newAssess.push(assessments[i])
+      } else {
+        newAssess.push({ week: i+1, status: "unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" })
+      }
+    }
+    setAssess(newAssess)
   }
+  
+  const colorArray = () => {
+    setStatusColor(
+      assessments.map(assessment => {
+        if(assessment.status === "pending"){
+          return "bg-[yellow]"
+        } else if(assessment.status === "complete") {
+          return "bg-[green]"
+        } else if(assessment.status === "incomplete") {
+          return "bg-[red]"
+        } else if (assessment.status === "unassigned") {
+          return "bg-[gray]"
+        }
+      })
+    )
+  }
+
+  useEffect(() => {
+    assessmentsUpdate(),
+    colorArray()
+  }, [])
 
   return (
     <>
-      {assess.map((assessment, index) => {
-        handleStatusColor(assessment)
+      {assess.map((assessment, index) => {        
         return (
-          <div id="week" key={index} className="w-1/3 border-b border-black py-10">
+          <div id="week" key={index} className="w-full border-b border-black py-10">
             <div className="flex w-full flex-row justify-between">
               <div className="flex flex-col">
                 <div className="my-1 font-bold">Week {assessment.week} </div>
@@ -49,13 +51,13 @@ const Assessment = (props) => {
               </div>
               <div className="flex flex-col">
                 <div className="flex w-full flex-row items-center">
-                  <span className={`mr-2 h-4 w-4 rounded-full${statusColor}`}/>
+                  <span className={`mr-2 h-4 w-4 rounded-full ${statusColor[index]}`}/>
                   <div className="my-1"><strong>Status:</strong> {assessment.status}</div>
                 </div>
                 <div className="my-1 pl-6"><strong>Reviewer:</strong> {assessment.reviewer} </div>
               </div>
             </div>
-            <div className="mt-2"><strong>Notes:</strong> <br/>{assessment.notes} </div>
+            <div className="mt-8"><strong>Notes:</strong> <br/>{assessment.notes} </div>
           </div>
         )
       })}
