099fd3ef9138ac366a2657eb290c498667552ad5
Genemichael25 <genemichael25@yahoo.com>
2022-09-08 17:45:12 -0700

    Break for separation

--- src/components/Absences.js
+++ src/components/Absences.js
@@ -16,7 +16,7 @@ const Absences = (props) => {
       <div className="flex items-center text-2xl font-medium" id="absence">
         Absences: <span className="ml-2 flex items-center justify-center text-red-600">{absence}</span>
       </div>
-      {/* <br></br> */}
+      <br></br>
     </>
   )
 }
