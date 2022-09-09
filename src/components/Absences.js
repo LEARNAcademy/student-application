5c26515ab3c77e3e416c931196055939c18c94a3
Genemichael25 <genemichael25@yahoo.com>
2022-09-08 17:40:39 -0700

    Finished progress bar for absences

--- src/components/Absences.js
+++ src/components/Absences.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react"
-import Attendance from "./Attendance"
-
 
 const Absences = (props) => {
   const [absence, setAbsence] = useState(0)
@@ -18,10 +16,10 @@ const Absences = (props) => {
       <div className="flex items-center text-2xl font-medium" id="absence">
         Absences: <span className="ml-2 flex items-center justify-center text-red-600">{absence}</span>
       </div>
-      <br></br>
-      <Attendance />
+      {/* <br></br> */}
     </>
   )
 }
 
 export { Absences }
+      
--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -1,60 +1,36 @@
-import React from "react"
-// import { Absences } from "./Absences"
+import React, { useEffect, useState } from "react"
 
-const Attendance = () => {
-  //   const { student } = props
-  //   const [attendance, setAttendance] =useState(100)
-    
-  //   const attendanceBar = () => {
-  //      if (student.absences === 0)
-  //       setAttendence(attendence * 15 / 15)
-  //      else (sudent.absences)
-  //  }
-  
+const Attendance = ({absences}) => {
+  let [statusColor, setStatusColor] = useState([ "bg-red-600", "bg-red-600", "bg-red-600", "bg-orange-500", "bg-orange-500", "bg-orange-500", "bg-yellow-300", "bg-yellow-300", "bg-yellow-300", "bg-green-500", "bg-green-500", "bg-green-500", "bg-green-600", "bg-green-600", "bg-green-600" ])
+
+  const absenceUpdate = (abs) => {
+    let updateAbsence = [...statusColor]
+    for(let i = statusColor.length - Math.ceil(3 * abs); i < statusColor.length; i++) {
+      updateAbsence[i] = "bg-white"
+    }
+    setStatusColor(updateAbsence)
+  }
+
+  useEffect(() => {
+    absenceUpdate(absences)
+  }, [])
+
   return(
-    <>
-      <div className="inline-flex h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-        <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">
-          <div className="h-2.5 w-1/3 rounded-full bg-red-600"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-red-600"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-red-600"></div>
-        </div>
-        <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">          
-          <div className="h-2.5 w-1/3 rounded-full bg-orange-500"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-orange-500"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-orange-500"></div>
-        </div>
-        <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">          
-          <div className="h-2.5 w-1/3 rounded-full bg-yellow-300"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-yellow-300"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-yellow-300"></div>
-        </div>
-        <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">          
-          <div className="h-2.5 w-1/3 rounded-full bg-green-500"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-green-500"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-green-500"></div>
-        </div>
-        <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">          
-          <div className="h-2.5 w-1/3 rounded-full bg-green-600"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-white"></div>
-          <div className="h-2.5 w-1/3 rounded-full bg-white"></div>
-        </div>
+    <> 
+      <div className="inline-flex h-2.5 w-full rounded-full">
+        {statusColor.map((color, index) => {
+          return(
+            <div key={index} className={`h-2.5 w-1/3 rounded-full ${color}`}></div>
+          )})
+        }
       </div>
-      <div className="mb-1 flex justify-between">
-        <span className="text-sm font-medium text-red-700 dark:text-white">5</span>
-        <span className="text-sm font-medium text-yellow-400 dark:text-white">3</span>
-        <span className="text-base font-medium text-[#A670AF] dark:text-white">Awesome</span>
+      <div className="mb-1 mr-20 flex justify-between">
+        <span className="ml-2 font-medium text-red-700 dark:text-white">5</span>
+        <span className="ml-2 mr-24 font-medium text-yellow-400 dark:text-white">3</span>
+        <span className="ml-2 font-medium text-[#A670AF] dark:text-white">Awesome</span>
       </div>
+    
     </>
   )
 }
-export default Attendance
-
-// Absences = Absences + tardies
-                
-// tardies = tardies / 3
-
-// if Absences = 1/3, do this <div className="h-2.5 w-1/3 rounded-full bg-white"></div>
-// if Absences = 1 , do this <div className="inline-flex h-2.5 w-1/5 rounded-full bg-white">          
-
-
+export { Attendance }
