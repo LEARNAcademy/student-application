a030417e09dc1c79734e74fa844927946ed8bf10
Nicole <nicole.rivera061@gmail.com>
2022-09-09 19:56:30 -0700

    Login box now resizes with page resize. Spacing is more appropriate now.

--- src/pages/StudentLogin/StudentLogin.js
+++ src/pages/StudentLogin/StudentLogin.js
@@ -7,13 +7,13 @@ const StudentLogin = () => {
   return (
     <>
       <Header />
-      <div className="bg-[#313133] h-screen pt-52 pr-80 pl-80 font-['gesta']">
-        <div className="border-4 mx-36 h-80 border-transparent bg-fuchsia-400 rounded-lg">
-          <form className="flex flex-col gap-4 pt-10">
-            <div className="w-3/6 ml-[10rem]">
+      <div className="bg-[#313133] h-screen pt-52 font-['gesta']">
+        <div className="border-4 h-96 w-96 m-auto bg-fuchsia-400 rounded-lg">
+          <form className="flex flex-col justify-center items-centerborder-transparent pt-12">
+            <div className="w-52 m-auto mb-4">
               <div className="mb-2 block text-center">
                 <Label
-                  class="text-xl"
+                  class="text-2xl"
                   htmlFor="email1"
                   value="Your email" />
               </div>
@@ -23,10 +23,10 @@ const StudentLogin = () => {
                 placeholder="student@LEARN.com"
                 required={true} />
             </div>
-            <div className="w-3/6 ml-[10rem]">
+            <div className="w-52 m-auto mb-2">
               <div className="mb-2 block text-center">
                 <Label
-                  class="text-xl"
+                  class="text-2xl"
                   htmlFor="password1"
                   value="Your password" />
               </div>
@@ -35,13 +35,13 @@ const StudentLogin = () => {
                 type="password"
                 required={true} />
             </div>
-            <div className="flex items-center gap-2 ml-[16rem]">
+            <div className="flex items-center gap-2 m-auto mt-4 mb-6">
               <Checkbox id="remember" />
               <Label htmlFor="remember">
                 Remember me
               </Label>
             </div>
-            <div className="ml-[17rem]">
+            <div className="m-auto">
               <Button type="submit" class="bg-learngreen rounded-lg">
                 Submit
               </Button>
