c4942271cf14381484d80c32bfe937dd214f103b
DFWilliams <williams.dee.f99@gamil.com>
2022-09-12 10:09:00 -0700

    ✨[54] added Student Login Page,and Login Test Page

--- src/pages/StudentLogin/StudentLogin.js
+++ src/pages/StudentLogin/StudentLogin.js
@@ -7,15 +7,16 @@ const StudentLogin = () => {
   return (
     <>
       <Header />
-      <div className="bg-[#313133] h-screen pt-52 font-['gesta']">
-        <div className="border-4 h-96 w-96 m-auto bg-fuchsia-400 rounded-lg">
-          <form className="flex flex-col justify-center items-centerborder-transparent pt-12">
-            <div className="w-52 m-auto mb-4">
+      <div className="h-screen bg-slate-200 pt-52 font-['gesta']">
+        <div className="m-auto h-96 w-96 rounded-xl border-4 bg-zinc-700">
+          <form className="flex flex-col items-center justify-center border-transparent pt-12">
+            <div className="m-auto mb-4 w-52">
               <div className="mb-2 block text-center">
                 <Label
-                  class="text-2xl"
                   htmlFor="email1"
-                  value="Your email" />
+                > 
+                  <div className="text-2xl text-learnpurple"> Your email</div>
+                </Label>
               </div>
               <TextInput
                 id="email1"
@@ -23,28 +24,30 @@ const StudentLogin = () => {
                 placeholder="student@LEARN.com"
                 required={true} />
             </div>
-            <div className="w-52 m-auto mb-2">
+            <div className="m-auto mb-2 w-52">
               <div className="mb-2 block text-center">
                 <Label
-                  class="text-2xl"
                   htmlFor="password1"
-                  value="Your password" />
+                >
+                  <div className="text-2xl text-learnpurple">Your password</div>
+                </Label>
               </div>
               <TextInput
                 id="password1"
                 type="password"
                 required={true} />
             </div>
-            <div className="flex items-center gap-2 m-auto mt-4 mb-6">
+            <div className="m-auto mt-4 mb-6 flex items-center gap-2">
               <Checkbox id="remember" />
               <Label htmlFor="remember">
-                Remember me
+                <div className="text-learnpurple">Remember me</div>
               </Label>
             </div>
             <div className="m-auto">
-              <Button type="submit" class="bg-learngreen rounded-lg">
+              <Button type="submit" color="success" pill={true}>
                 Submit
               </Button>
+            
             </div>
           </form>
         </div>
