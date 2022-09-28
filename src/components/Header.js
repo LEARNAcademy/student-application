a6c68f8310db3fa615d3b36605dab2cd4340078b
ninerboy81 <105950535+ninerboy81@users.noreply.github.com>
2022-09-28 11:35:35 -0700

    fixed hamburger menu by adding custom utilites

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react"
 import learnLogo from "../assets/LEARN_Academy_logo-purple.png"
-import { Navigation } from  "./Navigation"
+import { Navigation } from "./Navigation"
 import { Greeter } from "../components/Greeter"
 
 const Header = (props) => {
   const { user } = props
-  const [isNavOpen, setIsNavOpen] = useState(false) 
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
   return (
     <>
       <div className="flex items-center justify-between border-b border-gray-400 bg-learngray py-2 sm:hidden">
         <div className="mr-44">
-          <img src={learnLogo} alt="logo" className="scale-[75%]"/>
+          <img src={learnLogo} alt="logo" className="scale-[75%]" />
         </div>
         <nav>
           <section className="mr-4 flex">
@@ -22,11 +23,8 @@ const Header = (props) => {
               <span className="block h-0.5 w-8 animate-pulse bg-learnpurple"></span>
               <span className="block h-0.5 w-8 animate-pulse bg-learnpurple"></span>
             </div>
-            {
-              isNavOpen 
-                ? <div className="hidden">
-                  : <div className="flex block absolute w-full h-[100vh] top-0 left-0 bg-white z-10 flex-col justify-evenly items-center">
-            }
+
+            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
               <div
                 className="absolute top-0 right-0 p-8"
                 onClick={() => setIsNavOpen(false)}
@@ -45,7 +43,7 @@ const Header = (props) => {
                 </svg>
               </div>
               <ul className="flex min-h-[250px] flex-col items-center justify-center">
-                <li><Greeter user={user}/></li>
+                <li><Greeter user={user} /></li>
                 <li className="my-8 border-b border-gray-400 text-lg uppercase">
                   <a href="/">Login</a>
                 </li>
@@ -58,7 +56,18 @@ const Header = (props) => {
         display: none;
       }
       .showMenuNav {
-        
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
       }
     `}</style>
       </div>
