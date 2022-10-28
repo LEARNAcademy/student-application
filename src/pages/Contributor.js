839faf6a560fa072a3d17e2efea38f1847be0660
fpsolis219 <fpsolis21995@gmail.com>
2022-10-28 16:48:11 -0700

    contributors svg icons applied & in progress

--- src/pages/Contributor.js
+++ src/pages/Contributor.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { contributors } from "../Helper"
-
-
+import github from "../assets/github.svg"
+import Linkedin from "../assets/Linkedin.svg"
 const Contributor = () => {
   return (
     <>
       <h1>Contributors</h1>
       {contributors.map((contributor) => (
-        <div key={contributor.id} className="w-full max-w-md rounded-lg border bg-learnpurple p-4 shadow-md dark:border-gray-700 dark:bg-gray-800 sm:p-8">
+        <div key={contributor.id} className="w-full max-w-md rounded-lg border bg-learnpurple p-4 shadow-md dark:border-gray-700 dark:bg-gray-800 sm:p-8 flex-wrap">
           <div className="mb-4 flex items-center justify-between">
             <h5 className="text-xl font-bold leading-none text-gray-900">{contributor.name}</h5>
-            <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-            Linked in Icon
+            <a href={contributor.linkedin} className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+            < img src={Linkedin}/>
             </a>
           </div>
           <div className="flow-root">
@@ -24,7 +24,9 @@ const Contributor = () => {
                     </p>
                   </div>
                   <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                          Github Icon
+                    <a href={contributor.gitHubb} className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                        < img src={github}/>
+                    </a>
                   </div>
                 </div>
               </li>
