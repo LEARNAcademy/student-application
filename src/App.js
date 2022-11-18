b0b189cf1f53d28daa32448eb039d6a902b733cd
ElijahBcodes <eli1inthesnow@gmail.com>
2022-11-18 11:21:20 -0800

    copy for aboutus

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { StudentLogin } from "./pages/StudentLogin/StudentLogin"
 import { Routes, Route, useNavigate } from "react-router-dom"
 import { Contributor } from "./pages/Contributor/Contributor"
 import { AboutUs } from "./pages/AboutUs/AboutUs"
-import { PrivacyPolicy} from "./pages/PrivacyPolicy/PrivacyPolicy"
+import { PrivacyPolicy } from "./pages/PrivacyPolicy/PrivacyPolicy"
 import { TermsOfUse } from "./pages/PrivacyPolicy/TermsOfUse/TermsOfUse"
-import { CodeOfConduct} from "./pages/CodeOfConduct/CodeOfConduct"
+import { CodeOfConduct } from "./pages/CodeOfConduct/CodeOfConduct"
 
 
 const App = () => {
@@ -23,8 +23,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<StudentLogin login={login} setLogin={setLogin} navigate={navigate} />} />
       <Route path="/studentdash" element={<StudentDashboard login={login} setLogin={setLogin} />} />
-      <Route path="/contributor" element={<Contributor/>} />
-      <Route path="/aboutus" element={<AboutUs/>} />
+      <Route path="/contributor" element={<Contributor />} />
+      <Route path="/aboutus" element={<AboutUs />} />
       <Route path="/privacypolicy" element={<PrivacyPolicy />} />
       <Route path="/termsofuse" element={<TermsOfUse />} />
       <Route path="/codeofconduct" element={<CodeOfConduct />} />
--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from "react"
+import { Header } from "../../components/Header"
+import  { Footer } from "../../components/Footer"
 
 const AboutUs = () => {
   return (
-    <p>About Us </p>
+    <>
+      <Header/>
+      <div className="mx-auto my-16 h-auto w-[90%] text-[#616161] ">
+        <h1 className="mt-4 mb-12 font-['gesta'] text-4xl text-learnpurple ">
+      About Us
+        </h1>
+        <p className="my-3 w-[60%]">
+      With equal access to knowledge and career development, LEARN academy is teaching a new generation of daring and diverse students to be compassionate, curious and professional web developers. (Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.)
+        </p>
+      </div>
+      <Footer/>
+    </>
+  
   )
 }
 
