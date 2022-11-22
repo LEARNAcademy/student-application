ab56929cda8c74fc92abaf9e76f7c94cd70698fe
ElijahBcodes <eli1inthesnow@gmail.com>
2022-11-22 08:48:47 -0800

    lint fix and indents fixed

--- src/pages/CodeOfConduct/CodeOfConduct.js
+++ src/pages/CodeOfConduct/CodeOfConduct.js
@@ -1,90 +1,90 @@
 import React from "react"
 import { Header } from "../../components/Header"
-import  { Footer } from "../../components/Footer"
+import { Footer } from "../../components/Footer"
 
 const CodeOfConduct = () => {
   return (
     <>
       <Header />
-      <h1 className="mt-4 mb-12 text-4xl font-bold text-learnpurple">             
-          Code of Conduct
+      <h1 className="mt-4 mb-12 text-4xl font-bold text-learnpurple">
+                Code of Conduct
       </h1>
       <p className="my-3">
-          At LEARN academy, we strive to be a positive voice in the San Diego Tech Community. Our goals are to be welcoming and inclusive and to provide a space for any person who wants to code.
-      </p> 
+                At LEARN academy, we strive to be a positive voice in the San Diego Tech Community. Our goals are to be welcoming and inclusive and to provide a space for any person who wants to code.
+      </p>
       <p className="my-3">
-          The administration at LEARN academy, endeavors to build a brave, inclusive space—online, in the classroom, and at our events. A primary assumption of a brave space is that everyone speaks with the positive intent of seeking greater knowledge and understanding of differing perspectives and experiences. To promote a spirit of inclusivity, we ask our instructors, students, mentors, and guests to follow a few foundational principles while sharing our space. These principles are embodied by, but not limited to, the following statements:
+                The administration at LEARN academy, endeavors to build a brave, inclusive space—online, in the classroom, and at our events. A primary assumption of a brave space is that everyone speaks with the positive intent of seeking greater knowledge and understanding of differing perspectives and experiences. To promote a spirit of inclusivity, we ask our instructors, students, mentors, and guests to follow a few foundational principles while sharing our space. These principles are embodied by, but not limited to, the following statements:
       </p>
       <h2 className="text-2xl text-learnblack">
-          We are Welcoming, Friendly, and Patient
+                We are Welcoming, Friendly, and Patient
       </h2>
       <p className="my-3">
-          We make sure that our actions do not obstruct others from their learning.
+                We make sure that our actions do not obstruct others from their learning.
       </p>
       <p className="my-3">
-          It is crucial that every individual knows that LEARN academy is an inclusive, respectful, community-oriented learning environment. Each individual in the LEARN academy community has a part to play in building and maintaining these principles.
+                It is crucial that every individual knows that LEARN academy is an inclusive, respectful, community-oriented learning environment. Each individual in the LEARN academy community has a part to play in building and maintaining these principles.
       </p>
       <h2 className="text-2xl text-learnblack">
-          We are Considerate
+                We are Considerate
       </h2>
       <p className="my-3">
-          Individuals work and decisions affect others. Whether writing code, giving a presentation or participating in a discussion, individuals’ actions have consequences. Individuals should take those consequences into account when making decisions.
-      </p> 
-      <h2 className="text-2xl text-learnblack">
-          We are Respectful
-      </h2>
-      <p className="my-3">
-          We can all experience frustration now and then; coding is hard work and can be exasperating. How can we ensure that frustration does not get in the way of positive, productive relationships with others? Give yourself grace if you feel frustrated with coding, another member of our community, or with yourself. LEARN staff is available at any time for individuals who need to talk through frustration or need help facilitating communication with another member of the community.
+                Individuals work and decisions affect others. Whether writing code, giving a presentation or participating in a discussion, individuals’ actions have consequences. Individuals should take those consequences into account when making decisions.
       </p>
       <h2 className="text-2xl text-learnblack">
-          We are Mindful of Vocabulary
+                We are Respectful
       </h2>
       <p className="my-3">
-          Remember that sexist, racist, and other exclusionary jokes or media content can be offensive to others. Consider the content of the music you play, the videos you stream, the images you share, and the language you use in our learning space. Do they contain words or images that some may find offensive or denigrating? Be kind to others, and do not insult or put down another person. Mindfulness often means reevaluating what we consider permissible.
+                We can all experience frustration now and then; coding is hard work and can be exasperating. How can we ensure that frustration does not get in the way of positive, productive relationships with others? Give yourself grace if you feel frustrated with coding, another member of our community, or with yourself. LEARN staff is available at any time for individuals who need to talk through frustration or need help facilitating communication with another member of the community.
       </p>
       <h2 className="text-2xl text-learnblack">
-          We use our Skills for Good
+                We are Mindful of Vocabulary
       </h2>
       <p className="my-3">
-          Computers are a revolutionary communication tool, which connects us like never before. With this connectivity comes a responsibility to respect others’ property and rights on the internet. Our duty goes beyond following the law, which is a must. At LEARN academy, we use our facility, resources, and skills to do “good” in the community. We help others make the internet safe as well, by following best practices of netizenship, such as reporting issues found on the internet or within our community in a manner consistent with LEARN academy’s values.
+                Remember that sexist, racist, and other exclusionary jokes or media content can be offensive to others. Consider the content of the music you play, the videos you stream, the images you share, and the language you use in our learning space. Do they contain words or images that some may find offensive or denigrating? Be kind to others, and do not insult or put down another person. Mindfulness often means reevaluating what we consider permissible.
       </p>
       <h2 className="text-2xl text-learnblack">
-          Reporting Inappropriate Behavior
+                We use our Skills for Good
       </h2>
       <p className="my-3">
-          We care about individuals’ experiences while they are with us at LEARN academy. Chelsea and Bryan are trained in conflict resolution and diversity and inclusion issues, and they are available to talk with individuals about any topic. If an individual’s experience as a member of the LEARN academy community has made them uncomfortable, feel unwelcome, or has in any other way not met their expectations of a professional learning environment, we are here to listen and to protect the brave space.
+                Computers are a revolutionary communication tool, which connects us like never before. With this connectivity comes a responsibility to respect others’ property and rights on the internet. Our duty goes beyond following the law, which is a must. At LEARN academy, we use our facility, resources, and skills to do “good” in the community. We help others make the internet safe as well, by following best practices of netizenship, such as reporting issues found on the internet or within our community in a manner consistent with LEARN academy’s values.
       </p>
       <h2 className="text-2xl text-learnblack">
-          At LEARN academy, we promise:
+                Reporting Inappropriate Behavior
       </h2>
       <p className="my-3">
-          To take all harassment reports seriously.
+                We care about individuals’ experiences while they are with us at LEARN academy. Chelsea and Bryan are trained in conflict resolution and diversity and inclusion issues, and they are available to talk with individuals about any topic. If an individual’s experience as a member of the LEARN academy community has made them uncomfortable, feel unwelcome, or has in any other way not met their expectations of a professional learning environment, we are here to listen and to protect the brave space.
+      </p>
+      <h2 className="text-2xl text-learnblack">
+                At LEARN academy, we promise:
+      </h2>
+      <p className="my-3">
+                To take all harassment reports seriously.
       </p>
       <p className="my-3">
-          To respect individuals’ privacy.
+                To respect individuals’ privacy.
       </p>
       <p className="my-3">
-          To remove the source of harassment. The process begins with a warning to the offender, followed by removing them from the area if the harassment continues.
+                To remove the source of harassment. The process begins with a warning to the offender, followed by removing them from the area if the harassment continues.
       </p>
       <p className="my-3">
-          Individuals can reach us in person, via Slack (including private messaging), and via the contact information listed below:
+                Individuals can reach us in person, via Slack (including private messaging), and via the contact information listed below:
       </p>
       <p className="my-3">
-          Chelsea Kaufman: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Chelsea@learnacademy.org">Chelsea@learnacademy.org</a>
+                Chelsea Kaufman: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Chelsea@learnacademy.org">Chelsea@learnacademy.org</a>
       </p>
       <p className="my-3">
-          Sarah Proctor: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Sarah@learnacademy.org">Sarah@learnacademy.org</a>
+                Sarah Proctor: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Sarah@learnacademy.org">Sarah@learnacademy.org</a>
       </p>
       <p className="my-3">
-          Sources: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.djangoproject.com/conduct/" rel="noreferrer">Django Code of Conduct</a>, <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://geekfeminism.fandom.com/wiki/Conference_anti-harassment/Responding_to_reports" rel="noreferrer">Geek Feminism Wiki – Responding to Reports</a>, <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://github.com/sumeetjain/code_of_conduct" rel="noreferrer">Omaha Code School – Code Of Conduct</a>
-      </p> 
-      <p className="my-3">
-          Learn more about brave spaces here: Arao, Brian and Kristi Clemens. “Safe Spaces to Brave Spaces: A New Way to Frame Dialogue Around Diversity and Social Justice.” The Art of Effective Facilitation, edited by <strong>Lisa M. Landreman</strong>, Stylus Publishing, 2013, pp. 135-150. <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.gvsu.edu/cms4/asset/843249C9-B1E5-BD47-A25EDBC68363B726/from-safe-spaces-to-brave-spaces.pdf" rel="noreferrer">Click here to read more about it</a>.
+                Sources: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.djangoproject.com/conduct/" rel="noreferrer">Django Code of Conduct</a>, <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://geekfeminism.fandom.com/wiki/Conference_anti-harassment/Responding_to_reports" rel="noreferrer">Geek Feminism Wiki – Responding to Reports</a>, <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://github.com/sumeetjain/code_of_conduct" rel="noreferrer">Omaha Code School – Code Of Conduct</a>
       </p>
       <p className="my-3">
-          More about netizenship (<a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://en.wikipedia.org/wiki/Netizen" rel="noreferrer">https://en.wikipedia.org/wiki/Netizen</a>)
-      </p>  
-      <Footer/>
+                Learn more about brave spaces here: Arao, Brian and Kristi Clemens. “Safe Spaces to Brave Spaces: A New Way to Frame Dialogue Around Diversity and Social Justice.” The Art of Effective Facilitation, edited by <strong>Lisa M. Landreman</strong>, Stylus Publishing, 2013, pp. 135-150. <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.gvsu.edu/cms4/asset/843249C9-B1E5-BD47-A25EDBC68363B726/from-safe-spaces-to-brave-spaces.pdf" rel="noreferrer">Click here to read more about it</a>.
+      </p>
+      <p className="my-3">
+                More about netizenship (<a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://en.wikipedia.org/wiki/Netizen" rel="noreferrer">https://en.wikipedia.org/wiki/Netizen</a>)
+      </p>
+      <Footer />
     </>
   )
 }
