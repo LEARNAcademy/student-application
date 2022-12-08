67652178c100b898035c33ba6f72fdd1894a4af6
Deanna "Will" Williams <deannawilliams@Deannas-MacBook-Pro.local>
2022-12-08 10:14:23 -0800

    added font to tailwind config and made changes in code to reflect the addition

--- src/pages/TermsOfUse/TermsOfUse.js
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -7,7 +7,7 @@ const TermsOfUse = () => {
   return (
     <>
       <Header />
-      <div className="mx-auto my-16 h-auto w-[90%] text-[#616161]">
+      <div className="mx-auto my-16 h-auto w-[90%] text-learncarbon">
         <h1 className="mt-4 mb-12 text-4xl font-semibold text-learnblack">
                     Terms & Conditions
         </h1>
