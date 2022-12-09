e83bc2f789f5cfabca84409638b1baa28ec0c8bc
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-12-08 16:27:27 -0800

    fixed margins within footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,49 @@
 import React from "react" 
 import learnSymbol from "../assets/learn-symbol.svg"
-import { NavLink } from "react-router-dom"
+// import { NavLink } from "react-router-dom"
 
 const Footer = () => {
   return (
     <>
-      <div className="h-auto w-full px-8 py-4 text-lg lg:px-8">
-        <div className="justify-start mx-20 space-y-4  sm:m-8 sm:mx-20  sm:flex sm:flex-wrap  sm:space-x-4 sm:space-y-4 sm:justify-self-auto sm:font-['gesta'] md:mx-16">
-          
-          <a href="/"  className="ml-4"> 
-            < img src={learnSymbol}/>
+      <center className="h-auto w-full px-8 py-4 text-lg lg:px-8">
+        <center className="sm:flex sm:justify-evenly font-['gesta']">
+          <a href="/" className="mb-4 sm:mb-0">
+            <img src={learnSymbol} className="mb-3 mr-3 h-8 justify-center" alt="Learn Logo" />
           </a>
-          <p className="flex-auto"> © 2022 LEARN</p>
+          <ul className="mb-6 flex flex-wrap items-center sm:mb-0 justify-center">
+            <li>
+              <a href="/aboutus" className="mr-4 hover:text-learnpurple md:mr-6 ">About Us</a>
+            </li>
+            <li>
+              <a href="/privacypolicy" className="mr-4 hover:text-learnpurple md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/termsofuse" className="mr-4 hover:text-learnpurple md:mr-6 ">Terms of Use</a>
+            </li>
+            <li>
+              <a href="/codeofconduct" className="mr-4 hover:text-learnpurple">Code of Conduct</a>
+            </li>
+            <li>
+              <a href="/contributors" className="mr-4 hover:text-learnpurple">Contributors</a>
+            </li>
+          </ul>
+        </center>
+        <hr className="my-6 border-black sm:mx-auto lg:my-8" />
+        <span className="block text-center "> <a href="/" className="hover:text-learnpurple">© 2022 LEARN</a>
+        </span>
+        {/* <div className="justify-start ml-2 space-y-4 space-x-2  sm:mx-20 sm:flex sm:flex-wrap  sm:space-x-[96px] sm:space-y-20 sm:justify-self-auto sm:font-['gesta'] md:ml-4 md:mx-12 md:text-sm ">
+          
+          < img src={learnSymbol}/>
+          <a href="/"  className="ml-[33px]sm:ml-4"> 
+          </a>
+          <p className="flex mt-[-55px] ml-[50px] mb-[25px] sm:flex-auto"> © 2022 LEARN</p>
           <NavLink to="/aboutus" className="hover:text-learnpurple">About Us</NavLink>
           <NavLink to="/privacypolicy" className="hover:text-learnpurple">Privacy Policy</NavLink>
           <NavLink to="/termsofuse" className="hover:text-learnpurple">Terms of Use</NavLink>
           <NavLink to="/codeofconduct" className="hover:text-learnpurple">Code of Conduct</NavLink>
           <NavLink to="/contributor" className="hover:text-learnpurple">Contributors</NavLink>
-        </div>
-      </div>
+        </div> */}
+      </center>
     </>
   )}
 
