121652183b355db16bbd6f6fdc76faf884770a1c
Shadow090509 <jamoso090509@gmail.com>
2023-02-14 09:21:49 -0800

    💄 resolved comment on styling link

--- src/components/AssessmentProcessModal/AssessmentProcessModal.js
+++ src/components/AssessmentProcessModal/AssessmentProcessModal.js
@@ -44,7 +44,7 @@ const AssessmentProcessModal = () => {
               <div className="h-[60vh] space-y-6 overflow-y-auto p-6 pt-2 font-gesta">
                 <p>Being a developer requires an acute attention to detail. The expectation of this assessment is that all instructions are read carefully and followed completely.</p>
                 <p>A good developer will ask clarifying questions when a task is unclear and use all the resources at their disposal.</p>
-                <p>The step-by-step instructions <a href="https://github.com/LEARNAcademy/Syllabus/blob/main/github/assessments.md">here</a> breakdown the git and GitHub process for this assessment.</p>
+                <p>The step-by-step instructions <a href="https://github.com/LEARNAcademy/Syllabus/blob/main/github/assessments.md" className="text-purple-600 hover:text-purple-800 visited:text-purple-400"><strong><u>here</u></strong></a> breakdown the git and GitHub process for this assessment.</p>
                 <p>Assessment should be accessed through the GitHub Classroom link provided to you in Slack. PRs should be posted <i>before</i> class begins on Monday.</p>
               </div>
             </Modal.Body>
