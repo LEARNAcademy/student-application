634a143e299579855ecdcedc817b9ed722a3df4b
Charlie19255 <charlie19255@gmail.com>
2023-02-27 16:14:09 -0800

    Added another space

--- src/pages/StudentLogin/StudentLogin.js
+++ src/pages/StudentLogin/StudentLogin.js
@@ -3,7 +3,7 @@ import { Header } from "../../components/Header/Header"
 import { Checkbox, Label, TextInput } from "flowbite-react"
 
 const StudentLogin = (props) => {
-  const { login, navigate,verifyLogin } = props
+  const { login, navigate, verifyLogin } = props
   const [field, setField] = useState({
     email: "",
     password: ""
