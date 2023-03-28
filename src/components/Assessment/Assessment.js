dfa1f8709bdc5163ed33e4d56193bad4b5ca10fb
Moss <mossc2001@gmail.com>
2023-03-28 14:17:07 -0700

    made edits to pull db assessment info rather than mock data

--- src/components/Assessment/Assessment.js
+++ src/components/Assessment/Assessment.js
@@ -1,85 +1,92 @@
 import React, { useState, useEffect } from "react"
 
-const Assessment = (props) => {
-  const { assessments } = props
-  const [ statusColor, setStatusColor] = useState([])
-  const [assess, setAssess] = useState([])
-  const [ compColor, setCompColor ] = useState([])
-
-  const assessmentsUpdate = () => {
-    const newAssess = []
-    for(let i = 0; i < 6 ; i++) {
-      if(assessments[i] && assessments[i].week === i + 1) {
-        newAssess.push(assessments[i])
-      } else {
-        newAssess.push({ week: i+1, status: "Unassigned", comprehension: "N/A", reviewer: "N/A", notes: "N/A" })
-      }
-    }
-    setAssess(newAssess)
-  }
-  
-  const colorArray = () => {
-    setStatusColor(
-      assessments.map(assessment => {
-        if(assessment.status === "Pending"){
-          return "bg-yellow-100"
-        } else if(assessment.status === "Complete") {
-          return "bg-green-200"
-        } else if(assessment.status === "Incomplete") {
-          return "bg-red-300"
-        } else if (assessment.status === "Unassigned") {
-          return "bg-gray-300"
-        }
-      })
-    )
-  }
-
-  const compColorArray = () => {
-    setCompColor(
-      assessments.map(assessment => {
-        if(assessment.comprehension === "Incomplete"){
-          return "bg-red-300"
-        } else if(assessment.comprehension === "Complete") {
-          return "bg-green-200"
-        } else if(assessment.comprehension === "Novice") {
-          return "bg-[#A670AF]/20"
-        } else if (assessment.comprehension === "Advanced Beginner") {
-          return "bg-[#A670AF]/20"
-        } else if (assessment.comprehension === "Competent") {
-          return "bg-[#A670AF]/20"
-        }
-      })
-    )
-  }
+const Assessment = ({ currentUser }) => {
+  const [assessments, setAssessments] = useState([])
 
   useEffect(() => {
-    assessmentsUpdate(),
-    colorArray(),
-    compColorArray()
-  }, [])
-
+    if (currentUser?.assessment) {
+      setAssessments(currentUser?.assessment)
+    }
+  }, [currentUser])
+  
   return (
     <div className="px-12 sm:px-32">
-      {assess.map((assessment, index) => {        
+      {assessments.map((assessment, index) => {
         return (
-          <div id="week" key={index} className="w-full border-t border-black py-10">
+          <div
+            id="week"
+            key={index}
+            className="w-full border-t border-black py-10"
+          >
             <div className="w-full flex-row justify-between md:flex lg:flex xl:flex">
               <div className="flex flex-col">
-                <div className="my-1 text-xl font-bold">Week {assessment.week} </div>
-                <div className="my-1 text-lg"><strong>Comprehension:</strong>
-                  <span className={`mx-1 inline-block w-28 rounded-lg text-center  ${compColor[index]}`}>{assessment.comprehension}</span>
+                <div className="my-1 text-xl font-bold">
+                  Week {assessment.week}{" "}
+                </div>
+                <div className="my-1 text-lg">
+                  <strong>Comprehension:</strong>
+                  {assessment.comprehension === 0 && (
+                    <span className="mx-1 inline-block w-28 rounded-lg bg-red-300 text-center">
+                      Incomplete
+                    </span>
+                  )}
+                  {assessment.comprehension === 1 && (
+                    <span className="mx-1 inline-block w-28 rounded-lg bg-green-200 text-center">
+                      Complete
+                    </span>
+                  )}
+                  {assessment.comprehension === 2 && (
+                    <span className="mx-1 inline-block w-28 rounded-lg bg-[#A670AF]/20 text-center">
+                      Novice
+                    </span>
+                  )}
+                  {assessment.comprehension === 3 && (
+                    <span className="mx-1 inline-block w-28 rounded-lg bg-[#A670AF]/20 text-center">
+                      Advanced Beginner
+                    </span>
+                  )}
+                  {assessment.comprehension === 4 && (
+                    <span className="mx-1 inline-block w-28 rounded-lg bg-[#A670AF]/20 text-center">
+                      Competent
+                    </span>
+                  )}
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex w-full flex-row items-center">
-                  <div className="mx-6 ml-16 text-lg xxs:m-0 xs:m-0 sm:m-0 md:my-1 lg:my-1 xl:my-1"><strong>Status:</strong>
-                    <span className={`mx-1 inline-block w-28 rounded-lg text-center ${statusColor[index]}`}>{assessment.status}</span>
+                  <div className="mx-6 ml-16 text-lg xxs:m-0 xs:m-0 sm:m-0 md:my-1 lg:my-1 xl:my-1">
+                    <strong>Status:</strong>
+                    {assessment.status === 0 && (
+                      <span className="mx-1 inline-block w-28 rounded-lg bg-red-300 text-center">
+                        Incomplete
+                      </span>
+                    )}
+                    {assessment.status === 1 && (
+                      <span className="mx-1 inline-block w-28 rounded-lg bg-gray-300 text-center">
+                        Unassigned
+                      </span>
+                    )}
+                    {assessment.status === 2 && (
+                      <span className="mx-1 inline-block w-28 rounded-lg bg-yellow-100 text-center">
+                        Pending
+                      </span>
+                    )}
+                    {assessment.status === 3 && (
+                      <span className="mx-1 inline-block w-28 rounded-lg bg-green-200 text-center">
+                        Complete
+                      </span>
+                    )}
                   </div>
                 </div>
-                <div className="mx-6 ml-16 text-lg xxs:m-0 xs:m-0 sm:m-0 md:my-1 lg:my-1 xl:my-1"><strong>Reviewer:</strong> {assessment.reviewer} </div>
+                <div className="mx-6 ml-16 text-lg xxs:m-0 xs:m-0 sm:m-0 md:my-1 lg:my-1 xl:my-1">
+                  <strong>Reviewer:</strong> {assessment.reviewer}{" "}
+                </div>
               </div>
             </div>
-            <div className="mt-8 w-[30ch] text-base sm:w-[58ch]"><strong>Notes:</strong> <br/>{assessment.notes} </div>
+            <div className="mt-8 w-[30ch] text-base sm:w-[58ch]">
+              <strong>Notes:</strong> <br />
+              {assessment.notes}{" "}
+            </div>
           </div>
         )
       })}
