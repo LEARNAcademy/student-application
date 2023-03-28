88f3ff396a8ffaf6e084296fc2dd71f798a4e290
Moss <mossc2001@gmail.com>
2023-03-28 14:52:50 -0700

    fixed assessment test to work with updated passing of data

--- src/components/Assessment/Assessment.test.js
+++ src/components/Assessment/Assessment.test.js
@@ -1,13 +1,23 @@
 import React from "react"
 import { Assessment } from "./Assessment"
-import { render, queryAllByAttribute } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 
-const getAllById = queryAllByAttribute.bind(null, "id")
-const dom = render(<Assessment assessments={[]}/>)
-
-describe("<Assessment />", () => {
-  test("render 6 divs with the text Week", () => {
-    const week = getAllById(dom.container, "week")
-    expect(week).toHaveLength(6)
+describe ("<Assessment />", () => {
+  let currentUser = {
+    assessment: [
+      {
+        student_id: 1,
+        week: 1,
+        status: 1,
+        comprehension: 1,
+        reviewer: "",
+        notes: ""
+      }
+    ]
+  }
+  it("renders the weekly assessments", () => {
+    render(<Assessment currentUser={currentUser}/>)
+    const weeklyAssessment = screen.getByText(/week/i)
+    expect(weeklyAssessment).toBeInTheDocument()
   })
 })
