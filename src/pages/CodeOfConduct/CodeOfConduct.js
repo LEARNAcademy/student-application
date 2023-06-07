15e025415f43da7309ef03d788831d4841c3dbbb
Valeria <valeriacasillas123@yahoo.com>
2023-06-07 11:42:27 -0700

    :lipstick: configured paragraphs to max width 80 character

--- src/pages/CodeOfConduct/CodeOfConduct.js
+++ src/pages/CodeOfConduct/CodeOfConduct.js
@@ -7,89 +7,89 @@ const CodeOfConduct = () => {
         <h1 className="mt-4 mb-12 text-4xl font-semibold text-learnblack">
                 Code of Conduct
         </h1>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 At LEARN academy, we strive to be a positive voice in the San Diego Tech Community. Our goals are to be welcoming and inclusive and to provide a space for any person who wants to code.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 The administration at LEARN academy, endeavors to build a brave, inclusive space—online, in the classroom, and at our events. A primary assumption of a brave space is that everyone speaks with the positive intent of seeking greater knowledge and understanding of differing perspectives and experiences. To promote a spirit of inclusivity, we ask our instructors, students, mentors, and guests to follow a few foundational principles while sharing our space. These principles are embodied by, but not limited to, the following statements:
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 We are Welcoming, Friendly, and Patient
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 We make sure that our actions do not obstruct others from their learning.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 It is crucial that every individual knows that LEARN academy is an inclusive, respectful, community-oriented learning environment. Each individual in the LEARN academy community has a part to play in building and maintaining these principles.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 We are Considerate
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Individuals work and decisions affect others. Whether writing code, giving a presentation or participating in a discussion, individuals’ actions have consequences. Individuals should take those consequences into account when making decisions.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 We are Respectful
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 We can all experience frustration now and then; coding is hard work and can be exasperating. How can we ensure that frustration does not get in the way of positive, productive relationships with others? Give yourself grace if you feel frustrated with coding, another member of our community, or with yourself. LEARN staff is available at any time for individuals who need to talk through frustration or need help facilitating communication with another member of the community.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 We are Mindful of Vocabulary
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Remember that sexist, racist, and other exclusionary jokes or media content can be offensive to others. Consider the content of the music you play, the videos you stream, the images you share, and the language you use in our learning space. Do they contain words or images that some may find offensive or denigrating? Be kind to others, and do not insult or put down another person. Mindfulness often means reevaluating what we consider permissible.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 We use our Skills for Good
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Computers are a revolutionary communication tool, which connects us like never before. With this connectivity comes a responsibility to respect others’ property and rights on the internet. Our duty goes beyond following the law, which is a must. At LEARN academy, we use our facility, resources, and skills to do “good” in the community. We help others make the internet safe as well, by following best practices of netizenship, such as reporting issues found on the internet or within our community in a manner consistent with LEARN academy’s values.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 Reporting Inappropriate Behavior
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 We care about individuals’ experiences while they are with us at LEARN academy. Chelsea and Sarah are trained in conflict resolution and diversity and inclusion issues, and they are available to talk with individuals about any topic. If an individual’s experience as a member of the LEARN academy community has made them uncomfortable, feel unwelcome, or has in any other way not met their expectations of a professional learning environment, we are here to listen and to protect the brave space.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
                 At LEARN academy, we promise:
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 To take all harassment reports seriously.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 To respect individuals’ privacy.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 To remove the source of harassment. The process begins with a warning to the offender, followed by removing them from the area if the harassment continues.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Individuals can reach us in person, via Slack (including private messaging), and via the contact information listed below:
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Chelsea Kaufman: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Chelsea@learnacademy.org">Chelsea@learnacademy.org</a>
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
                 Sarah Proctor: <a className="text-learnpurple hover:text-learngreen active:text-learngreen" href="mailto:Sarah@learnacademy.org">Sarah@learnacademy.org</a>
         </p>
         <br></br>
-        <h3 className="text-lg text-learnblack">
+        <h3 className="text-lg text-learnblack max-w-readable">
                 Additional Resources:
         </h3>
         <ol className="ml-8 list-disc">
-          <li className="my-3"> <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.djangoproject.com/conduct/" rel="noreferrer">Code of Conduct - Django</a>
+          <li className="my-3 max-w-readable"> <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.djangoproject.com/conduct/" rel="noreferrer">Code of Conduct - Django</a>
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://geekfeminism.fandom.com/wiki/Conference_anti-harassment/Responding_to_reports" rel="noreferrer"> Conference Anti-Harassment/Responding to Reports - Geek Feminism</a>
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://github.com/sumeetjain/code_of_conduct" rel="noreferrer">Code of Conduct - Omaha Code School</a>
           </li>
         </ol>
-        <p className="my-3"><a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.naspa.org/files/dmfile/Policy_and_Practice_No_2_Safe_Brave_Spaces.pdf" rel="noreferrer">Read more about brave spaces here -</a> “Safe Spaces and Brave Spaces Historical Context and Recommendations for Student Affairs Professionals” NASPA Research and Policy Institute, edited by <strong>Diana Ali</strong>, NASPA, Issue 2 October 2017.
+        <p className="my-3 max-w-readable"><a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://www.naspa.org/files/dmfile/Policy_and_Practice_No_2_Safe_Brave_Spaces.pdf" rel="noreferrer">Read more about brave spaces here -</a> “Safe Spaces and Brave Spaces Historical Context and Recommendations for Student Affairs Professionals” NASPA Research and Policy Institute, edited by <strong>Diana Ali</strong>, NASPA, Issue 2 October 2017.
         </p>
-        <p className="my-3"> Read more about <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://en.wikipedia.org/wiki/Netizen" rel="noreferrer"> netizenship</a>.
+        <p className="my-3 max-w-readable"> Read more about <a className="text-learnpurple hover:text-learngreen active:text-learngreen" target="_blank" href="https://en.wikipedia.org/wiki/Netizen" rel="noreferrer"> netizenship</a>.
         </p>
       </div>
     </>
--- src/pages/TermsOfUse/TermsOfUse.js
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -7,14 +7,14 @@ const TermsOfUse = () => {
         <h1 className="mt-4 mb-12 text-4xl font-semibold text-learnblack">
           Terms & Conditions
         </h1>
-        <h2 className="text-2xl text-learnblack">Welcome to LEARN academy</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Welcome to LEARN academy</h2>
+        <p className="my-3 max-w-readable">
           These terms and conditions outline the rules and regulations for the
           use of LEARN academy&apos;s Website.
         </p>
-        <p className="my-3">LEARN academy is located at:</p>
+        <p className="my-3 max-w-readable">LEARN academy is located at:</p>
         <address>2986 Ivy Street, San Diego, CA 92104</address>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           <a
             className="text-learnpurple hover:text-learngreen active:text-learngreen"
             href="tel:619-789-6537"
@@ -22,13 +22,13 @@ const TermsOfUse = () => {
             (619)-789-6537
           </a>
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           By accessing this website we assume you accept these terms and
           conditions in full. Do not continue to use LEARN academy&apos;s
           website if you do not accept all of the terms and conditions stated on
           this page.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           The following terminology applies to these Terms and Conditions,
           Privacy Statement and Disclaimer Notice and any or all Agreements:
           &apos;Client&apos;, &apos;You&apos; and &apos;Your&apos; refers to
@@ -48,21 +48,21 @@ const TermsOfUse = () => {
           plural, capitalization and/or he/she or they, are taken as
           interchangeable and therefore as referring to same.
         </p>
-        <h2 className="text-2xl text-learnblack">Cookies</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Cookies</h2>
+        <p className="my-3 max-w-readable">
           We employ the use of cookies. By using LEARN academy&apos;s website
           you consent to the use of cookies in accordance with LEARN
           academy&apos;s privacy policy.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           Most of the modern day interactive web sites use cookies to enable us
           to retrieve user details for each visit. Cookies are used in some
           areas of our site to enable the functionality of this area and ease of
           use for those people visiting. Some of our affiliate/advertising
           partners may also use cookies.
         </p>
-        <h2 className="text-2xl text-learnblack">License</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">License</h2>
+        <p className="my-3 max-w-readable">
           Unless otherwise stated, LEARN academy and/or it&apos;s licensors own
           the intellectual property rights for all material on LEARN academy.
           All intellectual property rights are reserved. You may view and/or
@@ -78,47 +78,47 @@ const TermsOfUse = () => {
           for your own personal use subject to restrictions set in these terms
           and conditions.
         </p>
-        <p className="my-3 font-bold">You must not:</p>
+        <p className="my-3 font-bold max-w-readable">You must not:</p>
         <ol className="ml-7 list-decimal">
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Republish material from https://www.learnacademy.org
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Sell, rent or sub-license material from https://www.learnacademy.org
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Reproduce, duplicate or copy material from
             https://www.learnacademy.org
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Redistribute content from LEARN academy (unless the content is
             specifically made for redistribution)
           </li>
         </ol>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
           Hyperlinking to our Content
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           The following organizations may link to our Web site without prior
           written approval:
         </p>
-        <ol className="ml-8 list-disc">
-          <li className="my-3">Government agencies</li>
-          <li className="my-3">Search engines</li>
-          <li className="my-3">News organizations</li>
-          <li className="my-3">
+        <ol className="ml-8 list-disc max-w-readable">
+          <li className="my-3 max-w-readable">Government agencies</li>
+          <li className="my-3 max-w-readable">Search engines</li>
+          <li className="my-3 max-w-readable">News organizations</li>
+          <li className="my-3 max-w-readable">
             Online directory distributors when they list us in the directory may
             link to our Web site in the same manner as they hyperlink to the Web
             sites of other listed businesses
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Systemwide Accredited Businesses except soliciting non-profit
             organizations, charity shopping malls, and charity fundraising
             groups which may not hyperlink to our Web site
           </li>
         </ol>
         <ol start="2">
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             These organizations may link to our home page, to publications or to
             other Web site information so long as the link: (a) is not in any
             way misleading; (b) does not falsely imply sponsorship, endorsement
@@ -126,32 +126,32 @@ const TermsOfUse = () => {
             (c) fits within the context of the linking party&apos;s site.
           </li>
         </ol>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           We may consider and approve in our sole discretion other link requests
           from the following types of organizations:
         </p>
-        <ol className="ml-8 list-disc">
-          <li className="my-3">
+        <ol className="ml-8 list-disc max-w-readable">
+          <li className="my-3 max-w-readable">
             Commonly-known consumer and/or business information sources such as
             Chambers of Commerce, American Automobile Association, AARP and
             Consumers Union
           </li>
-          <li className="my-3">Dot.com community sites</li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">Dot.com community sites</li>
+          <li className="my-3 max-w-readable">
             Associations or other groups representing charities, including
             charity giving sites
           </li>
-          <li className="my-3">Online directory distributors</li>
-          <li className="my-3">Internet portals</li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">Online directory distributors</li>
+          <li className="my-3 max-w-readable">Internet portals</li>
+          <li className="my-3 max-w-readable">
             Accounting, law and consulting firms whose primary clients are
             businesses
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Educational institutions and trade associations
           </li>
         </ol>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           We will approve link requests from these organizations if we determine
           that: (a) the link would not reflect unfavorably on us or our
           accredited businesses (for example, trade associations or other
@@ -164,19 +164,19 @@ const TermsOfUse = () => {
           content in a newsletter or similar product furthering the mission of
           the organization.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           These organizations may link to our home page, to publications or to
           other Web site information so long as the link: (a) is not in any way
           misleading; (b) does not falsely imply sponsorship, endorsement or
           approval of the linking party and it products or services; and (c)
           fits within the context of the linking party&apos;s site.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           If you are among the organizations listed in paragraph 2 above and are
           interested in linking to our website, you must notify us by sending an
           e-mail to{" "}
           <a
-            className="text-learnpurple hover:text-learngreen active:text-learngreen"
+            className="text-learnpurple hover:text-learngreen active:text-learngreen max-w-readable"
             href="mailto:hello@learnacademy.org"
           >
             hello@learnacademy.org
@@ -187,32 +187,32 @@ const TermsOfUse = () => {
           site, and a list of the URL(s) on our site to which you would like to
           link. Allow 2-3 weeks for a response.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           Approved organizations may hyperlink to our Web site as follows:
         </p>
-        <ol className="ml-8 list-disc">
-          <li className="my-3">By use of our corporate name</li>
-          <li className="my-3">
+        <ol className="ml-8 list-disc max-w-readable">
+          <li className="my-3 max-w-readable">By use of our corporate name</li>
+          <li className="my-3 max-w-readable">
             By use of the uniform resource locator (Web address) being linked to
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             By use of any other description of our Web site or material being
             linked to that makes sense within the context and format of content
             on the linking party&apos;s site.
           </li>
         </ol>
-        <p className="my-3 font-bold">
+        <p className="my-3 font-bold max-w-readable">
           No use of LEARN academy&apos;s logo or other artwork will be allowed
           for linking absent a trademark license agreement.
         </p>
-        <h2 className="text-2xl text-learnblack">Iframes</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Iframes</h2>
+        <p className="my-3 max-w-readable">
           Without prior approval and express written permission, you may not
           create frames around our Web pages or use other techniques that alter
           in any way the visual presentation or appearance of our Web site.
         </p>
-        <h2 className="text-2xl text-learnblack">Reservation of Rights</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Reservation of Rights</h2>
+        <p className="my-3 max-w-readable">
           We reserve the right at any time and in its sole discretion to request
           that you remove all links or any particular link to our Web site. You
           agree to immediately remove all links to our Web site upon such
@@ -221,23 +221,23 @@ const TermsOfUse = () => {
           site, you agree to be bound to and abide by these linking terms and
           conditions.
         </p>
-        <h2 className="text-2xl text-learnblack">
+        <h2 className="text-2xl text-learnblack max-w-readable">
           Removal of links from our website
         </h2>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           If you find any link on our Web site or any linked web site
           objectionable for any reason, you may contact us about this. We will
           consider requests to remove links but will have no obligation to do so
           or to respond directly to you.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           Whilst we endeavor to ensure that the information on this website is
           correct, we do not warrant its completeness or accuracy; nor do we
           commit to ensuring that the website remains available or that the
           material on the website is kept up to date.
         </p>
-        <h2 className="text-2xl text-learnblack">Content Liability</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Content Liability</h2>
+        <p className="my-3 max-w-readable">
           We shall have no responsibility or liability for any content appearing
           on your Web site. You agree to indemnify and defend us against all
           claims arising out of or based upon your Website. No link(s) may
@@ -246,8 +246,8 @@ const TermsOfUse = () => {
           criminal, or which infringes, otherwise violates, or advocates the
           infringement or other violation of, any third party rights.
         </p>
-        <h2 className="text-2xl text-learnblack">Disclaimer</h2>
-        <p className="my-3">
+        <h2 className="text-2xl text-learnblack max-w-readable">Disclaimer</h2>
+        <p className="my-3 max-w-readable">
           To the maximum extent permitted by applicable law, we exclude all
           representations, warranties, and conditions relating to our website
           and the use of this website (including, without limitation, any
@@ -255,25 +255,25 @@ const TermsOfUse = () => {
           for purpose and/or the use of reasonable care and skill). Nothing in
           this disclaimer will:
         </p>
-        <ol className="ml-8 list-disc">
-          <li className="my-3">
+        <ol className="ml-8 list-disc max-w-readable">
+          <li className="my-3 max-w-readable">
             Limit or exclude our or your liability for death or personal injury
             resulting from negligence
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Limit or exclude our or your liability for fraud or fraudulent
             misrepresentation
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Limit any of our or your liabilities in any way that is not
             permitted under applicable law
           </li>
-          <li className="my-3">
+          <li className="my-3 max-w-readable">
             Exclude any of our or your liabilities that may not be excluded
             under applicable law.
           </li>
         </ol>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           The limitations and exclusions of liability set out in this Section
           and elsewhere in this disclaimer: (a) are subject to the preceding
           paragraph; and (b) govern all liabilities arising under the disclaimer
@@ -281,7 +281,7 @@ const TermsOfUse = () => {
           liabilities arising in contract, in tort (including negligence) and
           for breach of statutory duty.
         </p>
-        <p className="my-3">
+        <p className="my-3 max-w-readable">
           To the extent that the website and the information and services on the
           website are provided free of charge, we will not be liable for any
           loss or damage of any nature.
