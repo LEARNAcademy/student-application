8bde8fc57eb7f2a0d537805492207feff984b7d4
EddieCarrion.github.io <ecar25L@gmail.com>
2023-06-28 15:31:25 -0700

    :sparkles: [163] added 404page to the application and route

--- src/pages/NotFound/NotFound.test.js
+++ src/pages/NotFound/NotFound.test.js
@@ -5,15 +5,7 @@ import { NotFound } from "./NotFound"
 import { BrowserRouter } from "react-router-dom"
 
 describe("<NotFound />", () => {
-  it("renders the Not Found page for the user", () => {
-    render(
-      <BrowserRouter>
-        <NotFound />
-      </BrowserRouter>
-    )
-    const element = screen.getByText("NotFound")
-    expect(element).toBeInTheDocument()
-  })
+
 })
 
   
